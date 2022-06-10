5da9da75a3a3916f158fa6117592d39488db5f9c
ngvinh3258 <84054376+ngvinh3258@users.noreply.github.com>
2022-06-10 09:28:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" class="container">
+    <ProgressBar :step="step" />
+    <StepWrap :step="step" />
+    <FormWrap :userData="userData" :step="step" @next-click="nextStep" @pre-click="preStep" @reset-click="resetHandle"
+      @send-click="sendData" />
+
+  </div>
+</template>
+
+<script>
+import FormWrap from './components/FormWrap.vue'
+import StepWrap from './components/StepWrap.vue';
+import ProgressBar from './components/ProgressBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FormWrap,
+    StepWrap,
+    ProgressBar
+  },
+  data() {
+    return {
+      userData: {
+        name: '', email: '', company: '', employees: '', selected: '', acceptTerm: false
+
+      },
+      step: 1
+    }
+  },
+  methods: {
+    nextStep() {
+      console.log("nextStep", this.step)
+      this.step++;
+    },
+    preStep() {
+      if (this.step !== 1) {
+        this.step--;
+      }
+    },
+    resetHandle() {
+      this.step = 1;
+      //resetData
+    }
+    , sendData() {
+      console.log("userData", this.userData)
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-top: 60px;
+  padding: 60px 20px 0 20px;
+}
+
+.container {
+  background-color: #e4e6e8;
+  height: calc(100vh - 60px);
+}
+</style>
